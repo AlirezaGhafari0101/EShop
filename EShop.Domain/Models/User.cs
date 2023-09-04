8eac007dbd1a43b4b59bac3d10e6143c533fbb9c
Saeed_Pasha <hajizadesaeed.78@gmail.com>
2023-09-04 06:22:04 -0700

    The project template was configured

--- EShop.Domain/Models/User.cs
+++ EShop.Domain/Models/User.cs
@@ -11,7 +11,21 @@ namespace EShop.Domain.Models
     {
         [Key]
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        [Required]
+        [StringLength(50)]
+        [MaxLength(6)]
         public string Password { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Avatar { get; set; }
+        public string ActiveCode { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
