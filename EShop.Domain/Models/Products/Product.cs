5d5b3c86a24000fe56ea2f87c0061a08725cf31f
Alireza <alirezaghafari0101@gmail.com>
2023-09-17 17:38:38 +0330

    update Product Model

--- EShop.Domain/Models/Products/Product.cs
+++ EShop.Domain/Models/Products/Product.cs
@@ -1,6 +1,7 @@
 ﻿using EShop.Domain.common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,15 @@ namespace EShop.Domain.Models.Products
 {
     public class Product: BaseEntity
     {
+        [MaxLength(200)]
+        public string Title{ get; set; }
 
+        public string Description { get; set; }
+  
+        public string Tag { get; set; }
+
+        public string Image { get; set; }
+
+        public short Count { get; set; }
     }
 }
