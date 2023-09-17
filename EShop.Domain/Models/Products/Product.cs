4c20997a3fa3baa14a3a4e06133a7813eb366f22
Alireza <alirezaghafari0101@gmail.com>
2023-09-17 17:27:32 +0330

    create product model

---
+++ EShop.Domain/Models/Products/Product.cs
@@ -0,0 +1,14 @@
+﻿using EShop.Domain.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.Models.Products
+{
+    public class Product: BaseEntity
+    {
+
+    }
+}
