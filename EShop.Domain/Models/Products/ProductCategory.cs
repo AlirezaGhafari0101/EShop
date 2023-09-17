0fc39f4676ca87d113cfc97236e6759e9df57719
Saeed_Pasha <hajizadesaeed.78@gmail.com>
2023-09-17 05:39:51 -0700

    Create ProductCategory Model

---
+++ EShop.Domain/Models/Products/ProductCategory.cs
@@ -0,0 +1,37 @@
+﻿using EShop.Domain.common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.Models.Products
+{
+    public class ProductCategory:BaseEntity
+    {
+      
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string GroupTitle { get; set; }
+
+
+        [Display(Name = "گروه اصلی")]
+        public int? ParentId { get; set; }
+
+
+        #region Relations
+        [ForeignKey("ParentId")]
+        public List<ProductCategory> ProductCategories { get; set; }
+
+
+        [InverseProperty("ProductCategory")]
+        public List<Product> CateGories { get; set; }
+
+        [InverseProperty("Category")]
+        public List<Product> SubCategories { get; set; }
+        #endregion
+
+    }
+}
