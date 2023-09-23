884418d392aa09d34f94209a8bfa0cc0f86dba43
Alireza <alirezaghafari0101@gmail.com>
2023-09-23 22:10:57 +0330

    add discount controller and view

---
+++ EShop.Web/Areas/Admin/Controllers/DiscountController.cs
@@ -0,0 +1,26 @@
+﻿using EShop.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Areas.Admin.Controllers
+{
+    public class DiscountController : BaseController
+    {
+
+        private readonly IDiscountService _discountService;
+        public DiscountController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var discounts = await _discountService.GetAllDiscountsServiceAsync();
+            return View(discounts);
+        }
+        [Route("/AddDiscount")]
+        public async Task<IActionResult> AddDiscount()
+        {
+           
+            return View();
+        }
+    }
+}
