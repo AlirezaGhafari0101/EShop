7cbf45928a86e3b9ec623dcd97eb4a054a0b6b61
Alireza <alirezaghafari0101@gmail.com>
2023-09-26 17:43:29 +0330

    add productCategoriesCopmonent to show categories

---
+++ EShop.Web/Components/ProductCategoriesComponent.cs
@@ -0,0 +1,20 @@
+﻿using EShop.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Web.Components
+{
+    public class ProductCategoriesComponent: ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public ProductCategoriesComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("/Views/Components/ProductCategoriesComponent.cshtml");
+        }
+    }
+}
