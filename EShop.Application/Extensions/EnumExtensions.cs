722132952765aec951b35bad415c89e7d38bb431
Saeed_Pasha <hajizadesaeed.78@gmail.com>
2023-10-23 17:27:58 +0330

    show paymentType enum display name

---
+++ EShop.Application/Extensions/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Application.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumDisplayName(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = Attribute.GetCustomAttribute(field, typeof(DisplayAttribute)) as DisplayAttribute;
+            return attribute?.Name ?? value.ToString();
+        }
+    }
+}
---
+++ EShop.Domain/Models/Banner/Banner.cs
@@ -0,0 +1,13 @@
+﻿using EShop.Domain.common;
+using System.ComponentModel.DataAnnotations;
+namespace EShop.Domain.Models.Banner
+{
+    public class Banner:BaseEntity
+    {
+        [Display(Name = "موقعیت بنر")]
+        [StringLength(50,ErrorMessage ="{0}نمی تواند بیشتر از {1}کاراکتر باشد")]
+        public string Position { get; set; }
+       
+        public string Image { get; set; }
+    }
+}
